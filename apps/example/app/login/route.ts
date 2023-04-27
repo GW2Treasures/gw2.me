395923a8db8dbd4cce4da44c9ae3e5fe7fc3f751
darthmaim <git@darthmaim.de>
2023-04-27 21:27:09 +0200

    Fix stuff

--- apps/example/app/login/route.ts
+++ apps/example/app/login/route.ts
@@ -2,6 +2,8 @@ import { db } from '@/lib/db';
 import { redirect } from 'next/navigation';
 import { NextRequest } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
   // make sure example app exists
   const user = await db.user.upsert({
