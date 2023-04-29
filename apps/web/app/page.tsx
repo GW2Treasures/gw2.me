52f7cf49b382d2719b86748b9315f29757ee828d
darthmaim <git@darthmaim.de>
2023-04-29 21:51:14 +0200

    Add WIP hero to landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,16 @@
 import styles from './page.module.css';
+import HeroImage from './gw2me_hero.svg?svgr';
 
 export default function HomePage() {
   return (
-    <div>
-      gw2.me
+    <div className={styles.hero}>
+      <div>
+        <div className={styles.title}>gw2.me</div>
+        <div className={styles.sub}>Securly manage your Guild Wars 2 API keys</div>
+      </div>
+      <div className={styles.image}>
+        <HeroImage/>
+      </div>
     </div>
   );
 }
