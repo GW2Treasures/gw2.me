44cbdf6b27227e0ec71e04c7998e0efa747fde64
darthmaim <git@darthmaim.de>
2023-05-03 11:52:58 +0200

    Show authorized apps

--- apps/web/app/profile/page.tsx
+++ apps/web/app/profile/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { getUser } from '@/lib/getUser';
 import { db } from '@/lib/db';
 import { Metadata } from 'next';
@@ -7,6 +8,7 @@ import Link from 'next/link';
 import { Headline } from '@gw2treasures/ui/components/Headline/Headline';
 import { Table } from '@gw2treasures/ui/components/Table/Table';
 import { LinkButton } from '@gw2treasures/ui/components/Form/Button';
+import { AuthorizationType } from '@gw2me/database';
 
 const getUserData = cache(async () => {
   const session = await getUser();
@@ -17,7 +19,15 @@ const getUserData = cache(async () => {
 
   const user = await db.user.findUnique({
     where: { id: session.id },
-    include: { sessions: true, providers: true },
+    include: {
+      sessions: true,
+      providers: true,
+      authorizations: {
+        where: { type: AuthorizationType.RefreshToken },
+        include: { application: { select: { name: true }}},
+        orderBy: { createdAt: 'desc' },
+      }
+    },
   });
 
   if(!user) {
@@ -79,7 +89,24 @@ export default async function ProfilePage() {
       </Table>
 
       <Headline id="applications">Authorized Apps</Headline>
-      Are you a developer? <Link href="/dev/applications">Manage your own apps</Link>.
+      <p>Are you a developer? <Link href="/dev/applications">Manage your own apps</Link>.</p>
+
+      <Table>
+        <thead>
+          <tr>
+            <th>Application</th>
+            <th>Last Active</th>
+          </tr>
+        </thead>
+        <tbody>
+          {user.authorizations.map((authorization) => (
+            <tr key={authorization.id}>
+              <td><img src={`/api/application/${authorization.applicationId}/image`} width={32} height={32} alt="" style={{ verticalAlign: -10, borderRadius: 2 }}/> {authorization.application.name}</td>
+              <td>{authorization.usedAt?.toISOString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
