ee7d986d821c220549cd65cd36d514ed1d2a048e
darthmaim <git@darthmaim.de>
2023-10-04 14:20:08 +0200

    Add tooltip for GW2 permissions

---
+++ apps/web/components/Permissions/Permission.tsx
@@ -0,0 +1,33 @@
+import { Tip } from '@gw2treasures/ui/components/Tip/Tip';
+import { FC } from 'react';
+
+export interface PermissionProps {
+  permission: string;
+}
+
+const descriptions: Record<string, string> = {
+  'account': 'Your account display name, ID, home world, and list of guilds.',
+  'inventories': 'Your account bank, material storage, recipe unlocks, and character inventories.',
+  'characters': 'Basic information about your characters.',
+  'tradingpost': 'Your Trading Post transactions.',
+  'wallet': 'Your account\'s wallet.',
+  'unlocks': 'Your wardrobe unlocks—skins, dyes, minipets, finishers, etc.—and currently equipped skins.',
+  'pvp': 'Your PvP stats, match history, reward track progression, and custom arena details.',
+  'builds': 'Your currently equipped specializations, traits, skills, and equipment for all game modes.',
+  'progression': 'Your achievements, dungeon unlock status, mastery point assignments, and general PvE progress.',
+  'guilds': 'Guilds\' rosters, history, and MOTDs for all guilds you are a member of.',
+};
+
+export const Permission: FC<PermissionProps> = ({ permission }) => {
+  const description = descriptions[permission];
+
+  if(!description) {
+    return permission;
+  }
+
+  return (
+    <Tip tip={description}>
+      <span>{permission}</span>
+    </Tip>
+  );
+};
--- apps/web/components/Permissions/PermissionList.tsx
+++ apps/web/components/Permissions/PermissionList.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import styles from './PermissionList.module.css';
+import { Permission } from './Permission';
 
 export interface PermissionListProps {
   permissions: string[];
@@ -9,7 +10,7 @@ export const PermissionList: FC<PermissionListProps> = ({ permissions }) => {
   return (
     <ul className={styles.list}>
       {permissions.map((permission) => (
-        <li key={permission}>{permission}</li>
+        <li key={permission}><Permission permission={permission}/></li>
       ))}
     </ul>
   );
