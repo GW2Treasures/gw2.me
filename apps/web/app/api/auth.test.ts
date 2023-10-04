bbe958954e1af97461812327ed236ffa0e9c0b49
darthmaim <git@darthmaim.de>
2023-10-04 11:07:01 +0200

    Add tests

---
+++ apps/web/app/api/auth.test.ts
@@ -0,0 +1,32 @@
+import { Scope } from '@gw2me/api';
+import { verifyScopes } from './auth';
+
+describe('API authorization', () => {
+  describe('verifyScopes', () => {
+    it('array', () => {
+      expect(verifyScopes([Scope.Identify], [])).toBe(true);
+      expect(verifyScopes([Scope.Identify], [Scope.Identify])).toBe(true);
+      expect(verifyScopes([Scope.Identify], [Scope.Email])).toBe(false);
+    });
+
+    it('every', () => {
+      expect(verifyScopes([Scope.Identify], { every: [] })).toBe(true);
+      expect(verifyScopes([Scope.Identify], { every: [Scope.Identify] })).toBe(true);
+      expect(verifyScopes([Scope.Identify], { every: [Scope.Email] })).toBe(false);
+    });
+
+    it('oneOf', () => {
+      expect(verifyScopes([Scope.Identify], { oneOf: [] })).toBe(true);
+      expect(verifyScopes([Scope.Identify], { oneOf: [Scope.Identify] })).toBe(true);
+      expect(verifyScopes([Scope.Identify], { oneOf: [Scope.Email] })).toBe(false);
+      expect(verifyScopes([Scope.Identify], { oneOf: [Scope.Email, Scope.Identify] })).toBe(true);
+    });
+
+    it('combination', () => {
+      expect(verifyScopes([Scope.Identify, Scope.Email], { every: [Scope.Identify], oneOf: [Scope.Email, Scope.GW2_Account] })).toBe(true);
+      expect(verifyScopes([Scope.Identify, Scope.Email], { every: [Scope.Identify], oneOf: [Scope.GW2_Account, Scope.GW2_Builds] })).toBe(false);
+      expect(verifyScopes([Scope.Identify, Scope.Email], { every: [Scope.Identify, Scope.GW2_Account], oneOf: [Scope.Email] })).toBe(false);
+      expect(verifyScopes([Scope.Identify, Scope.Email], { every: [Scope.Identify], oneOf: [Scope.Identify] })).toBe(true);
+    });
+  });
+});
