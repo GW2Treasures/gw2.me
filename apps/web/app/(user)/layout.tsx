b87f3bffc0d81799eb18658e340a6c50f63e026e
darthmaim <git@darthmaim.de>
2023-10-05 12:04:31 +0200

    Add descriptions to profile pages

--- apps/web/app/(user)/layout.tsx
+++ apps/web/app/(user)/layout.tsx
@@ -4,6 +4,7 @@ import { LinkButton } from '@gw2treasures/ui/components/Form/Button';
 import { ReactNode } from 'react';
 import { useSelectedLayoutSegment } from 'next/navigation';
 import { ActiveButtonClass, NavLayout } from '@/components/Layout/NavLayout';
+import { Separator } from '@gw2treasures/ui/components/Layout/Separator';
 
 export default function ProfileLayout({ children }: { children: ReactNode }) {
   const segment = useSelectedLayoutSegment();
@@ -14,7 +15,7 @@ export default function ProfileLayout({ children }: { children: ReactNode }) {
       <LinkButton appearance="menu" href="/accounts" icon="key" className={segment === 'accounts' ? ActiveButtonClass : undefined}>GW2 Accounts</LinkButton>
       <LinkButton appearance="menu" href="/providers" icon="profession" className={segment === 'providers' ? ActiveButtonClass : undefined}>Login Providers</LinkButton>
       <LinkButton appearance="menu" href="/applications" icon="gw2me-outline" className={segment === 'applications' ? ActiveButtonClass : undefined}>Applications</LinkButton>
-      <hr/>
+      <Separator/>
       <LinkButton appearance="menu" href="/dev/applications" icon="developer">Developer</LinkButton>
     </NavLayout>
   );
--- apps/web/app/(user)/profile/page.tsx
+++ apps/web/app/(user)/profile/page.tsx
@@ -6,6 +6,8 @@ import { redirect } from 'next/navigation';
 import { cache } from 'react';
 import { Headline } from '@gw2treasures/ui/components/Headline/Headline';
 import { LinkButton } from '@gw2treasures/ui/components/Form/Button';
+import { Steps } from '@/components/Steps/Steps';
+import Link from 'next/link';
 
 const getUserData = cache(async () => {
   const session = await getUser();
@@ -34,6 +36,14 @@ export default async function ProfilePage() {
   return (
     <div>
       <Headline id="profile" actions={<LinkButton href="/logout" external>Logout</LinkButton>}>{user.name}</Headline>
+
+      <p>Thank you for signing up to gw2.me.</p>
+
+      <Steps>
+        <div><Link href="/accounts/add">Add your Guild Wars 2 Accounts</Link> by adding API Keys.</div>
+        <div><Link href="/discover">Discover</Link> applications that support gw2.me.</div>
+        <div>Are your a developer? <Link href="/dev/applications">Manage your own applications</Link>.</div>
+      </Steps>
     </div>
   );
 }
