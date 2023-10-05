6e317cb5003853d50b8ae8552a0b9826694194e3
darthmaim <git@darthmaim.de>
2023-10-05 12:18:13 +0200

    Add dev layout

--- apps/web/app/dev/applications/page.tsx
+++ apps/web/app/dev/applications/page.tsx
@@ -2,8 +2,9 @@ import { db } from '@/lib/db';
 import { getUser } from '@/lib/getUser';
 import { LinkButton } from '@gw2treasures/ui/components/Form/Button';
 import { Headline } from '@gw2treasures/ui/components/Headline/Headline';
+import { Table } from '@gw2treasures/ui/components/Table/Table';
 import Link from 'next/link';
-import { notFound } from 'next/navigation';
+import { redirect } from 'next/navigation';
 import { cache } from 'react';
 
 export const dynamic = 'force-dynamic';
@@ -12,7 +13,7 @@ const getApplications = cache(async () => {
   const user = await getUser();
 
   if(!user) {
-    notFound();
+    redirect('/login');
   }
 
   return db.application.findMany({ where: { ownerId: user.id }});
@@ -26,11 +27,20 @@ export default async function DevPage() {
     <div>
       <Headline id="applications" actions={<LinkButton href="/dev/applications/create">Create</LinkButton>}>Your Applications</Headline>
 
-      <ul>
-        {applications.map((app) => (
-          <li key={app.id}><Link href={`/dev/applications/${app.id}`}>{app.name}</Link></li>
-        ))}
-      </ul>
+      <Table>
+        <thead>
+          <tr>
+            <th>Application</th>
+          </tr>
+        </thead>
+        <tbody>
+          {applications.map((app) => (
+            <tr key={app.id}>
+              <td><Link href={`/dev/applications/${app.id}`}>{app.name}</Link></td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
---
+++ apps/web/app/dev/docs/page.tsx
@@ -0,0 +1,11 @@
+import { Headline } from '@gw2treasures/ui/components/Headline/Headline';
+
+export default function Docs() {
+  return (
+    <div>
+      <Headline id="docs">Documentation</Headline>
+
+      <p>TODO</p>
+    </div>
+  );
+}
