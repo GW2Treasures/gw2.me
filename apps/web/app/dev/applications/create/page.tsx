6f24404755a4cadf6c3fd51721aaa31098c43aa5
darthmaim <git@darthmaim.de>
2023-10-05 16:01:00 +0200

    Handle errors in page create form

--- apps/web/app/dev/applications/create/page.tsx
+++ apps/web/app/dev/applications/create/page.tsx
@@ -2,16 +2,23 @@ import { createApplication } from '../_actions/create';
 import { Headline } from '@gw2treasures/ui/components/Headline/Headline';
 import { Button } from '@gw2treasures/ui/components/Form/Button';
 import { TextInput } from '@gw2treasures/ui/components/Form/TextInput';
+import { Form } from '@/components/Form/Form';
+import { FlexRow } from '@gw2treasures/ui/components/Layout/FlexRow';
+import { Label } from '@gw2treasures/ui/components/Form/Label';
 
 export default function CreateApplicationPage() {
   return (
     <div>
       <Headline id="create">Create new application</Headline>
 
-      <form action={createApplication}>
-        <TextInput name="name"/>
-        <Button type="submit">Submit</Button>
-      </form>
+      <Form action={createApplication}>
+        <Label label="Name">
+          <TextInput name="name"/>
+        </Label>
+        <FlexRow>
+          <Button type="submit">Submit</Button>
+        </FlexRow>
+      </Form>
     </div>
   );
 }
