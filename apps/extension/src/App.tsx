816cae554133f52814e1a78b4edb0f1f8dbb5315
darthmaim <git@darthmaim.de>
2023-10-18 22:16:09 +0200

    Load accounts

--- apps/extension/src/App.tsx
+++ apps/extension/src/App.tsx
@@ -1,93 +1,142 @@
 import { FC, useCallback, useEffect, useState } from 'react';
 import { Button } from '@gw2treasures/ui/components/Form/Button';
-import { Gw2MeClient, Scope } from '@gw2me/client';
+import { AccountsResponse, Scope } from '@gw2me/client';
 import { client } from './client';
 
 export interface AppProps {
   // TODO: add props
 }
 
+enum Step {
+  INITIAL,
+  LOADING_ACCESS_TOKEN,
+
+  AUTH_REQUIRED,
+  AUTH_IN_PROGRESS,
+  AUTH_FAILED,
+
+  LOADING_ACCOUNTS,
+  READY,
+}
+
+function isLoadingStep(step: Step): step is Step.INITIAL | Step.LOADING_ACCESS_TOKEN | Step.AUTH_IN_PROGRESS | Step.LOADING_ACCOUNTS {
+  return step === Step.INITIAL || step === Step.LOADING_ACCESS_TOKEN || step === Step.AUTH_IN_PROGRESS || step === Step.LOADING_ACCOUNTS;
+}
+
 type State = {
-  loading: true,
+  step: Step.INITIAL | Step.LOADING_ACCESS_TOKEN | Step.AUTH_REQUIRED | Step.AUTH_IN_PROGRESS | Step.AUTH_FAILED,
 } | {
-  loading: false,
-  error?: boolean;
-  access_token: undefined,
-} | {
-  loading: false,
+  step: Step.LOADING_ACCOUNTS,
   access_token: string,
+} | {
+  step: Step.READY,
+  access_token: string,
+  accounts: AccountsResponse['accounts']
 };
 
 export const App: FC<AppProps> = ({ }) => {
-  const [state, setState] = useState<State>({ loading: true });
+  const [state, setState] = useState<State>({ step: Step.INITIAL });
+  console.log(state);
 
   useEffect(() => {
     const loadAccessToken = async () => {
       const value = await self.chrome.storage.sync.get('access_token');
 
-      setState({ loading: false, access_token: value.access_token })
+      if('access_token' in value && value.access_token) {
+        setState({ step: Step.LOADING_ACCOUNTS, access_token: value.access_token });
+      } else {
+        setState({ step: Step.AUTH_REQUIRED });
+      }
     }
 
-    loadAccessToken()
-  }, []);
+    if(state.step === Step.INITIAL) {
+      setState({ step: Step.LOADING_ACCESS_TOKEN })
+      loadAccessToken()
+    }
+  }, [state]);
 
+  useEffect(() => {
+    if(state.step === Step.LOADING_ACCOUNTS) {
+      (window as any).client = client;
+      (window as any).access_token = state.access_token;
+
+      client.api(state.access_token).accounts().then((({ accounts }) => {
+        setState({ step: Step.READY, access_token: state.access_token, accounts });
+      }));
+    }
+  }, [state])
 
   const login = useCallback(async () => {
-    setState({ loading: true });
+    setState({ step: Step.AUTH_IN_PROGRESS });
 
     const token = await setup();
 
     if(!token) {
-      setState({ loading: false, access_token: undefined, error: true });
+      setState({ step: Step.AUTH_FAILED });
       return;
     }
 
     const { access_token } = token;
 
     await self.chrome.storage.sync.set({ access_token });
-    setState({ loading: false, access_token })
+    setState({ step: Step.LOADING_ACCOUNTS, access_token })
   }, []);
 
-  if(state.loading) {
-    return <>Loading...</>
+  if(isLoadingStep(state.step)) {
+    return <>Loading... ({state.step}) <a onClick={() => chrome.storage.sync.clear().then(() => setState({ step: Step.INITIAL }))}>Reset</a></>
   }
 
   return (
     <div>
-      {!state.access_token ? (
+      {state.step === Step.AUTH_FAILED && (
+        <div>Authentication failed</div>
+      )}
+      {(state.step === Step.AUTH_REQUIRED || state.step === Step.AUTH_FAILED) && (
         <Button onClick={login} icon="gw2me">Login</Button>
-      ) : (
-        <>{JSON.stringify(state)}</>
+      )}
+      {state.step === Step.READY && (
+        <ul>
+          {state.accounts.map((account) => (
+            <li key={account.id}>{account.name}</li>
+          ))}
+        </ul>
       )}
     </div>
   );
 };
 
 async function setup() {
-  var redirect_uri = chrome.identity.getRedirectURL();
-  console.log(redirect_uri);
+  try {
+    const redirect_uri = chrome.identity.getRedirectURL();
+    console.log(redirect_uri);
 
-  const { code_challenge, code_challenge_method, code_verifier } = await generatePKCEChallenge();
+    const { code_challenge, code_challenge_method, code_verifier } = await generatePKCEChallenge();
 
-  const callback = await chrome.identity.launchWebAuthFlow({
-    interactive: true,
-    url: client.getAuthorizationUrl({ redirect_uri, scopes: [Scope.GW2_Account], code_challenge, code_challenge_method })
-  });
+    const authUrl = client.getAuthorizationUrl({ redirect_uri, scopes: [Scope.GW2_Account], code_challenge, code_challenge_method });
 
-  if(!callback) {
+    const callback = await chrome.identity.launchWebAuthFlow({
+      interactive: true,
+      url: authUrl,
+    });
+
+    if(!callback) {
+      return undefined;
+    }
+
+    const url = new URL(callback);
+    const code = url.searchParams.get('code');
+
+    if(!code) {
+      return undefined;
+    }
+
+    const token = await client.getAccessToken({ code, redirect_uri, code_verifier });
+
+    return token;
+  } catch(error) {
+    console.log(error);
     return undefined;
   }
-
-  const url = new URL(callback);
-  const code = url.searchParams.get('code');
-
-  if(!code) {
-    return undefined;
-  }
-
-  const token = await client.getAccessToken({ code, redirect_uri, code_verifier });
-
-  return token;
 }
 
 async function generatePKCEChallenge() {
