d90f6fc2ed77f06dc1f573671e2fe7325239daba
darthmaim <git@darthmaim.de>
2023-10-20 13:09:52 +0200

    Update extension for Firefox

--- apps/extension/src/client.ts
+++ apps/extension/src/client.ts
@@ -1,3 +1,3 @@
 import { Gw2MeClient } from '@gw2me/client';
 
-export const client = new Gw2MeClient({ client_id: 'd81b650e-ef90-493a-97a3-871bfcb8063e' }, { url: 'http://localhost:4000/' });
+export const client = new Gw2MeClient({ client_id: 'd81b650e-ef90-493a-97a3-871bfcb8063e' });
