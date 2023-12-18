98e4357af94820c01fa67dbcf2f85a748eb318ce
darthmaim <git@darthmaim.de>
2023-12-18 16:47:47 +0100

    Add admin user detail page

--- apps/web/app/admin/users/page.tsx
+++ apps/web/app/admin/users/page.tsx
@@ -2,6 +2,8 @@ import { FormatDate } from '@/components/Format/FormatDate';
 import { Code } from '@/components/Layout/Code';
 import { PageLayout } from '@/components/Layout/PageLayout';
 import { db } from '@/lib/db';
+import { Icon } from '@gw2treasures/ui';
+import { LinkButton } from '@gw2treasures/ui/components/Form/Button';
 import { Headline } from '@gw2treasures/ui/components/Headline/Headline';
 import { createDataTable } from '@gw2treasures/ui/components/Table/DataTable';
 
@@ -33,6 +35,7 @@ export default async function AdminUserPage() {
         <Users.Column id="accounts" title="Accounts" sortBy={({ _count }) => _count.accounts} align="right">{({ _count }) => _count.accounts}</Users.Column>
         <Users.Column id="createdAt" title="Created At" sortBy="createdAt">{({ createdAt }) => <FormatDate date={createdAt}/>}</Users.Column>
         <Users.Column id="session" title="Last access" sortBy={({ sessions }) => sessions[0]?.lastUsed}>{({ sessions }) => sessions.length > 0 ? <FormatDate date={sessions[0].lastUsed}/> : '-'}</Users.Column>
+        <Users.Column id="action" title="Actions" small>{({ id }) => <LinkButton appearance="menu" href={`/admin/users/${id}`} iconOnly><Icon icon="eye"/></LinkButton>}</Users.Column>
       </Users.Table>
     </PageLayout>
   );
