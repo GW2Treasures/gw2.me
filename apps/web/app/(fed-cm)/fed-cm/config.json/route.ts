45ac5befe006958a09b870fe16664d69b6536a17
darthmaim <git@darthmaim.de>
2024-07-13 21:47:35 +0200

    Use absolute icon path

--- apps/web/app/(fed-cm)/fed-cm/config.json/route.ts
+++ apps/web/app/(fed-cm)/fed-cm/config.json/route.ts
@@ -1,7 +1,10 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import icon from '../../../icon.svg';
+import { getUrlFromRequest } from '@/lib/url';
 
-export function GET() {
+export function GET(request: NextRequest) {
+  const baseUrl = getUrlFromRequest(request);
+
   return NextResponse.json({
     accounts_endpoint: '/fed-cm/accounts',
     client_metadata_endpoint: '/fed-cm/client-metadata',
@@ -10,7 +13,7 @@ export function GET() {
     branding: {
       background_color: '#ffffff',
       color: '#B7000D',
-      icons: [{ url: icon.src }]
+      icons: [{ url: new URL(icon.src, baseUrl) }]
     }
   });
 }
