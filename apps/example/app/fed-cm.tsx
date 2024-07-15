235e2cc1fae2cf2d66633a851364130b541a59cd
darthmaim <git@darthmaim.de>
2024-07-15 12:47:56 +0200

    Add FedCM button mode to example app

--- apps/example/app/fed-cm.tsx
+++ apps/example/app/fed-cm.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { Gw2MeClient } from '@gw2me/client';
+import { Button } from '@gw2treasures/ui/components/Form/Button';
 import { useRouter } from 'next/navigation';
-import { useEffect, type FC } from 'react';
+import { useCallback, useEffect, useState, type FC } from 'react';
 
 export interface FedCmProps {
   gw2meUrl: string;
@@ -10,6 +11,22 @@ export interface FedCmProps {
 
 export const FedCm: FC<FedCmProps> = ({ gw2meUrl }) => {
   const router = useRouter();
+  const [supportsFedCmMode, setSupportsFedCmMode] = useState(false);
+
+  useEffect(() => {
+    let supportsFedCmMode = false;
+    try {
+      navigator.credentials.get({
+        identity: Object.defineProperty(
+          {}, 'mode', {
+            get () { supportsFedCmMode = true; }
+          }
+        )
+      } as any).catch(() => {});
+    } catch(e) {}
+
+    setSupportsFedCmMode(supportsFedCmMode);
+  }, []);
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -30,5 +47,17 @@ export const FedCm: FC<FedCmProps> = ({ gw2meUrl }) => {
     return () => abortController.abort();
   });
 
-  return null;
+  const handleFedCMButtonClick = useCallback(() => {
+    const gw2me = new Gw2MeClient({ client_id: 'example_client_id' }, { url: gw2meUrl });
+    gw2me.fedCM.request({ mode: 'button' }).then((credential) => {
+      if(credential) {
+        router.push(`/callback?code=${credential.token}`);
+      }
+    });
+  }, [gw2meUrl, router]);
+
+  return supportsFedCmMode ? (
+    <Button onClick={handleFedCMButtonClick} icon="gw2me">Login with gw2.me (FedCM)</Button>
+  ) : null;
 };
+
