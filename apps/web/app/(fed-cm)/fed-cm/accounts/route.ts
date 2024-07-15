eb1d88275aa02ba1a39b73321818338f916b77eb
darthmaim <git@darthmaim.de>
2024-07-15 09:22:52 +0200

    Set `approved_clients` in FedCM accounts endpoint

--- apps/web/app/(fed-cm)/fed-cm/accounts/route.ts
+++ apps/web/app/(fed-cm)/fed-cm/accounts/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getUser } from '@/lib/session';
 import accountIcon from './fed-cm-account.png';
 import { getUrlFromRequest } from '@/lib/url';
+import { db } from '@/lib/db';
 
 export async function GET(request: NextRequest) {
   const user = await getUser();
@@ -9,6 +10,17 @@ export async function GET(request: NextRequest) {
   if(!user) {
     return new Response(null, { status: 401 });
   }
+
+  const notExpired = {
+    OR: [
+      { expiresAt: { gte: new Date() }},
+      { expiresAt: null }
+    ]
+  };
+  const approvedApplications = await db.application.findMany({
+    where: { authorizations: { some: { type: 'AccessToken', userId: user.id, ...notExpired }}},
+    select: { clientId: true }
+  });
 
   const baseUrl = getUrlFromRequest(request);
 
@@ -18,7 +30,7 @@ export async function GET(request: NextRequest) {
       name: user.name,
       email: user.email ?? user.name,
       picture: new URL(accountIcon.src, baseUrl),
-      approved_clients: [],
+      approved_clients: approvedApplications.map(({ clientId }) => clientId),
     }]
   });
 }
