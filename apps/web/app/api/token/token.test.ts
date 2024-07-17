b3389a86605eca75e9422a46e8ef02f496da9109
darthmaim <git@darthmaim.de>
2024-07-17 16:22:35 +0200

    Fix token tests

--- apps/web/app/api/token/token.test.ts
+++ apps/web/app/api/token/token.test.ts
@@ -6,8 +6,8 @@ import { handleTokenRequest } from './token';
 describe('/api/token', () => {
   describe('errors', () => {
     it('Missing client_id', () => expect(handleTokenRequest({})).rejects.toBeOAuth2Error(OAuth2ErrorCode.invalid_request, 'Missing client_id'));
-    it('Missing grant_type', () => expect(handleTokenRequest({ client_id: 'test' })).rejects.toBeOAuth2Error(OAuth2ErrorCode.invalid_request, 'Invalid grant_type'));
-    it('Invalid grant_type', () => expect(handleTokenRequest({ client_id: 'test', grant_type: 'foo' })).rejects.toBeOAuth2Error(OAuth2ErrorCode.invalid_request, 'Invalid grant_type'));
+    it('Missing grant_type', () => expect(handleTokenRequest({ client_id: 'test' })).rejects.toBeOAuth2Error(OAuth2ErrorCode.unsupported_grant_type, 'Invalid grant_type'));
+    it('Invalid grant_type', () => expect(handleTokenRequest({ client_id: 'test', grant_type: 'foo' })).rejects.toBeOAuth2Error(OAuth2ErrorCode.unsupported_grant_type, 'Invalid grant_type'));
 
     describe('authorization_code', () => {
       it('Missing code', () => expect(handleTokenRequest({ client_id: 'test', grant_type: 'authorization_code' })).rejects.toBeOAuth2Error(OAuth2ErrorCode.invalid_request, 'Missing code'));
