7b696b645f69f6fc820c8f056fa340a2a6e9a883
darthmaim <git@darthmaim.de>
2024-09-07 11:52:55 +0200

    Improve preview for verify-email for clients without `<Preview>` support

--- packages/emails/emails/verify-email.tsx
+++ packages/emails/emails/verify-email.tsx
@@ -4,7 +4,7 @@ import Template, { styles } from './template';
 export default function VerifyEmailMail({ username = '{username}', verifyLink = 'https://gw2.me/emails/verify?token={token}' }: { username: string, verifyLink: string }) {
   return (
     <Template preview="Please verify your email">
-      <Text style={styles.text}>Hi {username},</Text>
+      <Text style={styles.text}>Hi {username}, </Text>
       <Text style={styles.text}>Please verify your email address by clicking the following link:</Text>
       <Text>
         <Link style={styles.text} href={verifyLink}>{verifyLink}</Link>
