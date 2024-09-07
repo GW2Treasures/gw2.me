88f3d5c315464fb8f41146c9cd5a49f137951f58
darthmaim <git@darthmaim.de>
2024-09-07 10:06:31 +0200

    Update admin test email page

--- apps/web/app/admin/email/page.tsx
+++ apps/web/app/admin/email/page.tsx
@@ -3,24 +3,45 @@ import { SubmitButton } from '@gw2treasures/ui/components/Form/Buttons/SubmitBut
 import { Headline } from '@gw2treasures/ui/components/Headline/Headline';
 import { sendMail } from '@/lib/mail';
 import TestEmail from '@gw2me/emails/test';
+import { Form, FormState } from '@gw2treasures/ui/components/Form/Form';
+import { TextInput } from '@gw2treasures/ui/components/Form/TextInput';
+import { FlexRow } from '@gw2treasures/ui/components/Layout/FlexRow';
+import { getFormDataString } from '@/lib/form-data';
+import { ensureUserIsAdmin } from '../admin';
 
-export default function EmailPage() {
+export default async function EmailPage() {
+  await ensureUserIsAdmin();
+
   return (
     <PageLayout>
       <Headline id="email">Email</Headline>
-      <form action={send}>
-        <SubmitButton icon="mail">Send Test mail</SubmitButton>
-      </form>
+      <Form action={send}>
+        <FlexRow>
+          <TextInput type={'email' as 'text'} name="email" defaultValue="darthmaim@gw2.me"/>
+          <SubmitButton icon="mail">Send Test mail</SubmitButton>
+        </FlexRow>
+      </Form>
     </PageLayout>
   );
 }
 
-async function send() {
+async function send(_: FormState, formData: FormData): Promise<FormState> {
   'use server';
+  await ensureUserIsAdmin();
 
-  const message = await sendMail('Test Mail', 'gw2me@darthmaim.de', <TestEmail/>);
+  const email = getFormDataString(formData, 'email');
 
-  console.log(message);
+  if(!email) {
+    return { error: 'Invalid email' };
+  }
+
+  try {
+    await sendMail('Test Mail', email, <TestEmail/>);
+  } catch(e) {
+    return { error: `Error: ${e}` };
+  }
+
+  return { success: 'Email sent' };
 }
 
 export const metadata = {
