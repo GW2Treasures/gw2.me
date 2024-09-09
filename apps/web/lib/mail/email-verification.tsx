bbb539e9fed5204763dc18be8f4e4a6b426d95de
darthmaim <git@darthmaim.de>
2024-09-09 11:26:29 +0200

    Use `getBaseUrlFromHeaders` as base for email verification link

--- apps/web/lib/mail/email-verification.tsx
+++ apps/web/lib/mail/email-verification.tsx
@@ -2,6 +2,7 @@ import VerifyEmailMail from '@gw2me/emails/verify-email';
 import { db } from '../db';
 import { sendMail } from '../mail';
 import { randomBytes } from 'crypto';
+import { getBaseUrlFromHeaders } from '../url';
 
 export async function sendEmailVerificationMail(emailId: string) {
   const verificationToken = generateEmailVerificationToken();
@@ -12,7 +13,7 @@ export async function sendEmailVerificationMail(emailId: string) {
     select: { email: true, user: { select: { name: true }}}
   });
 
-  const link = new URL(`/emails/verify?token=${verificationToken}`, process.env.GW2ME_URL ?? 'https://gw2.me/');
+  const link = new URL(`/emails/verify?token=${verificationToken}`, getBaseUrlFromHeaders());
 
   await sendMail(
     'Verify your email',
