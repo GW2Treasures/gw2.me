e2f9ddd734ab70a9dc531d713f42e6c7b3784a00
darthmaim <git@darthmaim.de>
2024-09-12 13:55:28 +0200

    Use datatable for /dev/applications

--- apps/web/app/dev/applications/page.tsx
+++ apps/web/app/dev/applications/page.tsx
@@ -5,7 +5,7 @@ import { getSessionOrRedirect } from '@/lib/session';
 import { LinkButton } from '@gw2treasures/ui/components/Form/Button';
 import { Headline } from '@gw2treasures/ui/components/Headline/Headline';
 import { FlexRow } from '@gw2treasures/ui/components/Layout/FlexRow';
-import { Table } from '@gw2treasures/ui/components/Table/Table';
+import { createDataTable } from '@gw2treasures/ui/components/Table/DataTable';
 import { cache } from 'react';
 
 export const dynamic = 'force-dynamic';
@@ -23,36 +23,25 @@ const getApplications = cache(async () => {
         select: { id: true },
         where: { OR: [{ expiresAt: { gte: new Date() }}, { expiresAt: null }], type: { in: ['AccessToken', 'RefreshToken'] }},
         distinct: 'userId'
-      }
+      },
     },
+    orderBy: { createdAt: 'asc' },
   });
 });
 
 export default async function DevPage() {
   const applications = await getApplications();
+  const Applications = createDataTable(applications, ({ id }) => id);
 
   return (
     <PageLayout>
       <Headline id="applications" actions={<LinkButton href="/dev/applications/create" icon="add">Create</LinkButton>}>Your Applications</Headline>
 
-      <Table>
-        <thead>
-          <tr>
-            <Table.HeaderCell>Application</Table.HeaderCell>
-            <Table.HeaderCell>Users</Table.HeaderCell>
-            <Table.HeaderCell small>Actions</Table.HeaderCell>
-          </tr>
-        </thead>
-        <tbody>
-          {applications.map((app) => (
-            <tr key={app.id}>
-              <td><FlexRow><ApplicationImage fileId={app.imageId}/>{app.name}</FlexRow></td>
-              <td>{app.authorizations.length}</td>
-              <td><LinkButton href={`/dev/applications/${app.id}`} icon="settings">Manage</LinkButton></td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+      <Applications.Table>
+        <Applications.Column id="app" title="Application" sortBy="name">{({ name, imageId }) => (<FlexRow><ApplicationImage fileId={imageId}/>{name}</FlexRow>)}</Applications.Column>
+        <Applications.Column id="users" title="Users" sortBy={({ authorizations }) => authorizations.length}>{({ authorizations }) => authorizations.length}</Applications.Column>
+        <Applications.Column id="actions" title="Actions" small>{({ id }) => (<LinkButton href={`/dev/applications/${id}`} icon="settings">Manage</LinkButton>)}</Applications.Column>
+      </Applications.Table>
     </PageLayout>
   );
 }
