8a5721fb16f9f9b1fc6e94e1372fa645b5fa9f38
darthmaim <git@darthmaim.de>
2025-05-17 10:02:07 +0200

    Install `@types/web`

--- apps/web/types.d.ts
+++ apps/web/types.d.ts
@@ -2,24 +2,6 @@
 /// <reference types="react/canary" />
 /// <reference types="react-dom/canary" />
 
-// TODO: this should not be necessary, because this is already defined in the above reference of @gw2treasures/ui/types
-declare module '*.svg?svgr' {
-  import React from 'react';
-  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
-  export default SVG;
-}
-
-interface Navigator {
-  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/Navigator/login) */
-  login: NavigatorLogin
-}
-
-/** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/NavigatorLoginn) */
-interface NavigatorLogin {
-  /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/NavigatorLogin/setStatus) */
-  setStatus(status: 'logged-in' | 'logged-out'): Promise<undefined>
-}
-
 interface Window {
   /** [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/IdentityProvider) */
   IdentityProvider: IdentityProvider
@@ -32,4 +14,3 @@ interface IdentityProvider {
 
   resolve(token: string, options?: { accountId: string }): undefined
 }
-
