90a0af22bb70135e3493d3e8f8b9890878c8259b
darthmaim <git@darthmaim.de>
2025-08-25 13:44:07 +0200

    Refactor FedCM endpoints to use `@fedcm/server` package

--- apps/web/app/(fed-cm)/fed-cm/client-metadata/route.ts
+++ apps/web/app/(fed-cm)/fed-cm/client-metadata/route.ts
@@ -1,38 +1,50 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { OAuth2ErrorCode } from '@/lib/oauth/error';
+import { OAuth2Error, OAuth2ErrorCode } from '@/lib/oauth/error';
 import { db } from '@/lib/db';
+import { IdentityAssertionErrorResponse, IdentityProviderClientMetadata } from '@fedcm/server';
+import { assert } from '@/lib/oauth/assert';
+import { getUrlFromRequest } from '@/lib/url';
 
-export async function GET(request: NextRequest) {
-  // get client_id query parameter
-  const clientId = request.nextUrl.searchParams.get('client_id');
+export async function GET(request: NextRequest): Promise<NextResponse<IdentityProviderClientMetadata | IdentityAssertionErrorResponse>> {
+  try {
+    // get client_id query parameter
+    const clientId = request.nextUrl.searchParams.get('client_id');
 
-  // make sure client_id is set
-  if(!clientId) {
-    return NextResponse.json(
-      { error: { code: OAuth2ErrorCode.invalid_request, details: 'client_id missing' }},
-      { status: 400 }
-    );
-  }
+    // make sure client_id is set
+    assert(clientId, OAuth2ErrorCode.invalid_request, { description: '`client_id` missing' });
 
-  // get client from db
-  const client = await db.client.findUnique({
-    where: { id: clientId },
-    select: {
-      application: { select: { privacyPolicyUrl: true, termsOfServiceUrl: true }}
+    // get client from db
+    const client = await db.client.findUnique({
+      where: { id: clientId },
+      select: {
+        application: { select: { privacyPolicyUrl: true, termsOfServiceUrl: true, imageId: true }}
+      }
+    });
+
+    assert(client, OAuth2ErrorCode.invalid_client, { description: 'Invalid `client_id`', httpStatus: 404 });
+
+    // return privacy policy and TOS urls
+    return NextResponse.json({
+      privacy_policy_url: client.application.privacyPolicyUrl || undefined,
+      terms_of_service_url: client.application.termsOfServiceUrl || undefined,
+      icons: client.application.imageId
+        ? [{ url: new URL(`/api/file/${client.application.imageId}`, getUrlFromRequest(request)), size: 128 }]
+        : undefined
+    });
+  } catch(error) {
+    if(error instanceof OAuth2Error) {
+      return NextResponse.json(
+        { error: { code: error.code, error: error.description }},
+        { status: error.httpStatus ?? 400 }
+      );
     }
-  });
 
-  // make sure app exists
-  if(!client) {
+    console.error('[fed-cm/client-metadata] error');
+    console.error(error);
+
     return NextResponse.json(
-      { error: { code: OAuth2ErrorCode.invalid_client, details: 'client_id not found' }},
-      { status: 404 }
+      { error: { code: 'server_error', error: 'Unknown error' }},
+      { status: 500 }
     );
   }
-
-  // return privacy policy and TOS urls
-  return NextResponse.json({
-    'privacy_policy_url': client.application.privacyPolicyUrl || undefined,
-    'terms_of_service_url': client.application.termsOfServiceUrl || undefined,
-  });
 }
