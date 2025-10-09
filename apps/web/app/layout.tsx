a809058ff6a770f8296766f7d1aa24f9248bef7e
darthmaim <git@darthmaim.de>
2025-10-09 20:43:19 +0200

    Revert "Add API disabled banner"
    This reverts commit 4a2d048fb501d9abcbee2759caadf5462af68b44.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -47,10 +47,6 @@ export default async function RootLayout({ children }: LayoutProps<'/'>) {
             </div>
           </div>
           <hr className={styles.border}/>
-          <aside data-nosnippet="true" style={{ padding: 16, background: 'var(--color-brand)', color: '#fff', textAlign: 'center', lineHeight: 1.5 }}>
-            The official Guild Wars 2 API is currently experiencing major issues and has been partially disabled.<br/>
-            It is currently not possible to add new API keys or verify accounts. Applications using gw2.me might not work properly.
-          </aside>
           <DataTableContext>
             {children}
           </DataTableContext>
