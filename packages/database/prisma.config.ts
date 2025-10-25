00751c03aa4493e93091363981dc0fe9e8508e94
darthmaim <git@darthmaim.de>
2025-10-25 21:52:32 +0200

    Configure datasource in prisma.config

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'prisma/config';
+import { defineConfig, env } from 'prisma/config';
 import { loadEnvFile } from 'node:process';
 import { existsSync } from 'node:fs';
+import { styleText } from 'node:util';
 
 // load .env
 if(existsSync('.env')) {
+  console.log(styleText('dim', '[@gw2me/database] load .env'));
   loadEnvFile('.env');
 }
 
 export default defineConfig({
   schema: './prisma/schema.prisma',
+  engine: 'classic',
+  datasource: {
+    url: env('DATABASE_URL'),
+  },
 });
