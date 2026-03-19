988035ea02f837772a36ece20e55dd73fbf1c472
darthmaim <git@darthmaim.de>
2026-03-19 17:20:00 +0100

    Use native `toBase64` if available

--- packages/client/src/base64.ts
+++ packages/client/src/base64.ts
@@ -3,9 +3,22 @@ export function base64urlEncode(input: Uint8Array | ArrayBuffer): string {
     ? new Uint8Array(input)
     : input;
 
+  // use native toBase64 if available
+  if (Uint8Array.prototype.toBase64 !== undefined) {
+    return data.toBase64({ alphabet: 'base64url', omitPadding: true });
+  }
+
+  // fallback to btoa
   return btoa(String.fromCharCode(...data))
     .replace(/\+/g, '-')
     .replace(/\//g, '_')
     .replace(/=+$/, '');
 }
 
+// toBase64 is supported in all browsers and most other runtimes, but is still missing in typescript
+declare global {
+  interface Uint8Array {
+    /** @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array/toBase64 */
+    toBase64(options?: { alphabet?: 'base64' | 'base64url', omitPadding?: boolean }): string,
+  }
+}
